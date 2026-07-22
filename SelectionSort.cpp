15466fd3275bc3a05063ba702eedd03a610430e7
jaswanthsuresh <jaswanthsuresh10@gmail.com>
2026-07-22 17:52:14 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+void selectionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            std::swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+int main() {
+    std::vector<int> numbers = {64, 25, 12, 22, 11};
+
+    selectionSort(numbers);
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
